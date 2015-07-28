c79131402a9ffc7cac87aa6f836f9ba165207721
RuiSheng Wu <ATeamMac2014@gmail.com>
2015-07-28 13:46:07 +0800

    Initial Commit

---
+++ sigslot/sigslot.h
@@ -0,0 +1,13 @@
+//
+//  sigslot.h
+//  sigslot
+//
+//  Created by MacWrs on 15/7/28.
+//  Copyright (c) 2015年 MacWrs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sigslot : NSObject
+
+@end
---
+++ sigslot/sigslot.m
@@ -0,0 +1,13 @@
+//
+//  sigslot.m
+//  sigslot
+//
+//  Created by MacWrs on 15/7/28.
+//  Copyright (c) 2015年 MacWrs. All rights reserved.
+//
+
+#import "sigslot.h"
+
+@implementation sigslot
+
+@end
